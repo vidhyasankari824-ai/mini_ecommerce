50b32a7134b287e9dd74114fee5a38d47706b150
Vidhya <vidhyasankari824@gmail.com>
2026-06-23 12:41:50 +0530

    fix 502 issue test

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return render_template("index.html")
+    return "Home working fine"
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
