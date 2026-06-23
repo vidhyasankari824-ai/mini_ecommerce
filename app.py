48fcf5d00a786af0fe13cdffe5057cc32e88eed7
Vidhya <vidhyasankari824@gmail.com>
2026-06-23 12:27:48 +0530

    fix render 502 issue

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Working fine"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
