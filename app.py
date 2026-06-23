31f4c86854a4618a49ad16f7c87f620e30f2b179
Vidhya <vidhyasankari824@gmail.com>
2026-06-23 12:15:10 +0530

    fix port binding issue

--- app.py
+++ app.py
@@ -252,3 +252,9 @@ if __name__ == "__main__":
 
     if __name__ == "__main__":
       app.run(host="0.0.0.0", port=10000)
+
+      import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
