46466335271518f67805c8fd17fd291ec4cfefc3
Vidhya <vidhyasankari824@gmail.com>
2026-06-24 11:41:22 +0530

    added routes

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
